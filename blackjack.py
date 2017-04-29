9c43b45443f66021221f1ec512324d3499428a3e
editrocchio <editrocchio@gmail.com>
2017-04-29 04:49:22 +0000

    Basic game finished.

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class Mechanics(object):
     
@@ -16,7 +17,7 @@ class Mechanics(object):
                 deck_vals.update({x + y:10})
 
         for y in suits:
-            deck_vals.update({'A' + y:[11,1]})
+            deck_vals.update({'A' + y:11})
         
         items = deck_vals.items()
         random.shuffle(items)
@@ -26,38 +27,28 @@ class Mechanics(object):
     def ace_check(self, card_set_p, card_set_d):
         p = 0
         d = 0
-        end = False
+        ace_count_p = 0
+        ace_count_d = 0
+        
         for card in card_set_p:
-            if 'A' not in card[0]:
-                p += int(card[1])
-            elif 'A' in card[0]:
-                if p + int(card[1][0]) <= 21:
-                    p += int(card[1][0])
-                elif p + int(card[1][0]) > 21:
-                    p += int(card[1][1])
-
+            p += int(card[1])
+            if 'A' in card[0]:
+                ace_count_p += 1
+                
         for card in card_set_d:
-            if 'A' not in card[0]:
-                d += int(card[1])
-            elif 'A' in card[0]:
-                if d + int(card[1][0]) <= 21:
-                    d += int(card[1][0])
-                elif d + int(card[1][0]) > 21:
-                    d += int(card[1][1])
+            d += int(card[1])
+            if 'A' in card[0]:
+                ace_count_d += 1
 
-        for card in card_set_p:
-            if 'A' in card[0] and p > 21 and end == False:
-                p -= 10
-                end = True
-            else:
-                pass
+        if ace_count_p > 0:
+            for num in range(ace_count_p):
+                if p > 21:
+                    p -= 10
 
-        for card in card_set_d:
-            if 'A' in card[0] and d > 21 and end == False:
-                d -= 10
-                end = True
-            else:
-                pass
+        if ace_count_d > 0:
+            for num in range(ace_count_d):
+                if d > 21:
+                    d -= 10
 
         return p, d
 
@@ -65,20 +56,39 @@ class Mechanics(object):
         global play
 
         p_checked, d_checked = self.ace_check(p_cards, d_cards)
-
+        
+        deck = []        
         disp = []
         for card in d_cards:
             disp.append(card[0])
+
+        print "Your total: " + str(p_checked)
+
+        if p_checked == 21 and d_checked == 21:
+            print "You and dealer both have 21... Push." + \
+                  "\n--------------------"
+            player.money += int(wager)
+            play = False
             
-        if p_checked == 21 and d_checked != 21:
-            print "Blackjack! You win. Dealer cards: " + str(disp)
+        elif p_checked == 21 and d_checked != 21:
+            print "Blackjack! You win. Dealer cards: " + str(disp) + \
+                  "\n--------------------"
+            player.money += int((wager*2))
             play = False
+            
         elif p_checked != 21 and d_checked == 21:
-            print "Dealer has blackjack, you lose. " + str(disp)
+            print "Dealer has blackjack, you lose. " + str(disp) + \
+                  "\n--------------------"
             play = False
+            
         elif p_checked > 21:
-            print "You bust. Dealer cards: " + str(disp) 
+            print "You bust. Dealer cards: " + str(disp) + \
+                  "\n--------------------"
             play = False
+            
+        else:
+            pass
+        
 
     def stand_check(self, p_cards, d_cards):
 
@@ -87,13 +97,21 @@ class Mechanics(object):
         print "Your total: " + str(p_checked) + "\nDealer total: " + str(d_checked)
             
         if p_checked == d_checked:
-            print "Push."
+            player.money += int(wager)
+            print "Push." + \
+                  "\n--------------------"
         elif p_checked > d_checked:
-            print "You win!"
+            player.money += int((wager*2))
+            print "You win!" + \
+                  "\n--------------------"
         elif d_checked > p_checked:
-            print "You lose."
+            player.money -= int(wager)
+            print "You lose." + \
+                  "\n--------------------"
         elif p_checked != 21 and d_checked == 21:
-            print "Dealer has blackjack, you lose."
+            player.money -= int(wager)
+            print "Dealer has blackjack, you lose." + \
+                  "\n--------------------"
 
 class Player(object):
 
@@ -103,6 +121,8 @@ class Player(object):
         self.money = money
            
     def choice(self):
+        global play
+        play = True
         while play:
             c = raw_input("Do you want to [h]it or [s]tand? ")
             if c.lower() == "h":
@@ -114,13 +134,35 @@ class Player(object):
                 print "Dealer cards: " + str(dealer.dealer_cards[0][0]) + \
                       ", " + str(dealer.dealer_cards[1][0])
                 dealer.self_deal(dealer.dealer_cards)
-                break
+                play = False
+
+    def bet(self):
+        betting = True
+        while betting:
+            try:
+                print "You have $" + str(self.money) + " remaining."
+                amount = raw_input("Enter wager: ")
+                if int(amount) > self.money:
+                    print "You don't have enough, you have $" + str(self.money) + \
+                          " remaining."
+                else:
+                    print "Wagering " + "$" + amount
+                    self.money -= int(amount)
+                    betting = False
+            except ValueError:
+                print "Please enter an integer."
+        
+        time.sleep(0.5)
+        
+        return amount        
                 
 class Dealer(object):
 
     dealer_cards = []
    
     def deal(self, created_deck):
+        player.player_cards[:] = []
+        dealer.dealer_cards[:] = []
         count = 0
         while count < 2:
             new_card = deck.pop(0)
@@ -138,20 +180,24 @@ class Dealer(object):
                 new_card = deck.pop(0)
                 self.dealer_cards.append(new_card)
                 print "Dealing... " + new_card[0]
+                time.sleep(0.5)
             elif d_checked > 21:
                 print "Dealer busts at " + str(d_checked)
+                player.money += int((wager*2))
                 break
             elif d_checked >= 17 and d_checked < 21:
                 print "Dealer stands at " + str(d_checked)
                 mechanics.stand_check(player.player_cards, dealer.dealer_cards)
                 break
             elif d_checked == 21:
-                print "Dealer has blackjack, you lose. " 
+                print "Dealer has blackjack, you lose. "
+                player.money -= int(wager)
                 break
 
     def display_cards(self, p_cards, d_cards):
         x = []
         y = []
+        disp_total = []
         for card in p_cards:
             x.append(card[0])
         for card in d_cards:
@@ -162,12 +208,16 @@ class Dealer(object):
            
         mechanics.win_loss(player.player_cards, dealer.dealer_cards)
 
+
 mechanics = Mechanics()
 player = Player(30)
 dealer = Dealer()
-deck = mechanics.create_deck()
 
-play = True
+while player.money > 0:
+    wager = player.bet()
+    wager = int(wager)
+    deck = mechanics.create_deck()
+    dealer.deal(deck)
+    player.choice()
 
-dealer.deal(deck)
-player.choice()
+print "You're out of money, game over."
